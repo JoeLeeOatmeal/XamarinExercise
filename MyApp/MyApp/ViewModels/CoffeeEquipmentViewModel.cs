36616ff40c0f347576e788d26ddd309f3367eb90
Joe <x31207joe@gmail.com>
2021-07-05 19:03:29 +0800

    init

---
+++ MyApp/MyApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyApp.ViewModels
+{
+    public class CoffeeEquipmentViewModel : BindableObject
+    {
+        public ICommand IncreaseCount { get; }
+        public ICommand ResetCount { get; }
+
+        int count = 0;
+        string countDisplay = "Click Me !";
+
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay) return;
+
+                countDisplay = value;
+                OnPropertyChanged(nameof(CountDisplay));
+            }
+        }
+
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+            ResetCount = new Command(OnReset);
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+
+        void OnReset()
+        {
+            count = 0;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+    }
+}
